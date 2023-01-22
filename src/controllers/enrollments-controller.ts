b284cb74b7ef3321d807eaa6513fdebb16121b9c
rafaelBporto <rafaelbporto88@gmail.com>
2023-01-22 09:01:18 -0300

    feat: verify CEP

---
+++ src/controllers/enrollments-controller.ts
@@ -0,0 +1,57 @@
+import { AuthenticatedRequest } from "@/middlewares";
+import enrollmentsService from "@/services/enrollments-service";
+import { Response } from "express";
+import httpStatus from "http-status";
+
+export async function getEnrollmentByUser(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+
+  try {
+    const enrollmentWithAddress = await enrollmentsService.getOneWithAddressByUserId(userId);
+
+    return res.status(httpStatus.OK).send(enrollmentWithAddress);
+  } catch (error) {
+    return res.sendStatus(httpStatus.NO_CONTENT);
+  }
+}
+
+export async function postCreateOrUpdateEnrollment(req: AuthenticatedRequest, res: Response) {
+
+  try {
+
+    const {cep} = req.body.address
+    await enrollmentsService.getAddressFromCEP(cep);
+
+    await enrollmentsService.createOrUpdateEnrollmentWithAddress({
+      ...req.body,
+      userId: req.userId,
+    });
+
+    return res.sendStatus(httpStatus.OK);
+  } catch (error) {
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+}
+
+export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response) {
+  const { cep } = req.query as Record<string, string>;
+
+  try {
+    const address = await enrollmentsService.getAddressFromCEP(cep);
+    
+    address.cidade = address.localidade;
+    delete address.cep;
+    delete address.ddd;
+    delete address.gia;
+    delete address.ibge;
+    delete address.localidade;
+    delete address.siafi;
+    
+    res.status(httpStatus.OK).send(address);
+  } catch (error) {
+    if (error.name === "NotFoundError") {
+      return res.sendStatus(httpStatus.NO_CONTENT);
+    }
+  }
+}
+
